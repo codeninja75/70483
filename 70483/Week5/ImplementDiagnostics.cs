09b6b77d1e4ec050215691d5cfbc2d8338d09019
Chris McLean <chris.w.mclean@gmail.com>
2019-07-25 10:05:42 -0500

    Week 5

--- 70483/Week5/ImplementDiagnostics.cs
+++ 70483/Week5/ImplementDiagnostics.cs
@@ -1,7 +1,43 @@
-﻿namespace DotNet.E70483.DebugAndSecurity
+﻿#define BOB
+using System;
+using System.Diagnostics;
+
+namespace DotNet.E70483.DebugAndSecurity
 {
     public class ImplementDiagnostics
     {
+        public void DoSomeDiag()
+        {
+#if BOB
+            Console.WriteLine("Bob is here!");
+#endif
 
+#if DEBUG
+            Console.WriteLine("We are debugging");
+#endif
+
+        }
+
+    }
+    public class CustomListener : TraceListener
+    {
+
+        
+        public  void Writex(string message, [System.Runtime.CompilerServices.CallerMemberName] string memberName = "",
+        [System.Runtime.CompilerServices.CallerFilePath] string sourceFilePath = "",
+        [System.Runtime.CompilerServices.CallerLineNumber] int sourceLineNumber = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string message) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteLine(string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
